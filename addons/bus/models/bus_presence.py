e3aeb033183131f2f5967aacd362a50da2dabdbb
goran.kliska <goran.kliska@decod.io>
2021-02-17 13:05:05 +0100

    Kaluga HOT fix on Prod

--- addons/bus/models/bus_presence.py
+++ addons/bus/models/bus_presence.py
@@ -49,6 +49,7 @@ class BusPresence(models.Model):
             if datetime.datetime.strptime(presence.last_presence, DEFAULT_SERVER_DATETIME_FORMAT) < last_presence:
                 values['last_presence'] = last_presence.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
             # Hide transaction serialization errors, which can be ignored, the presence update is not essential
+            # KGB Can we comment 2 lines below to avoid locking errors
             with tools.mute_logger('odoo.sql_db'):
                 presence.write(values)
         # avoid TransactionRollbackError
