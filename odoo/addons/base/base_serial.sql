0db94a715622baf315477c069a2785090dabc863
Marko <marko.falatec@ecodica.eu>
2025-10-28 11:14:18 +0100

    [FIX] : With new version of postgres 17 and up this extension is no longer supported

--- odoo/addons/base/base_serial.sql
+++ odoo/addons/base/base_serial.sql
@@ -7,7 +7,7 @@
 -------------------------------------------------------------------------
 CREATE EXTENSION IF NOT EXISTS "unaccent";
 CREATE EXTENSION IF NOT EXISTS "tablefunc";
-CREATE EXTENSION IF NOT EXISTS "adminpack";
+--CREATE EXTENSION IF NOT EXISTS "adminpack";
 CREATE EXTENSION IF NOT EXISTS "postgres_fdw";
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "ltree";
